3ef11e0650f75db10cdad74fa9c81005a0c05f21
EC2 Default User <ec2-user@ip-172-31-39-217.ap-northeast-1.compute.internal>
2020-03-01 08:19:07 +0000

    新規投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  
+  def new
+    
+  end
+  
+  def create
+   @post = Post.create(create_params)
+   @posts = Post.all
+  end
+  
+  def destroy
+   Post.find(params[:id]).destroy
+   @posts = Post.all
+  end
+  
+  private
+  def create_params
+  params.require(:post).permit(:content, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
-
+  get "posts/new" => "posts#new"
   get "/" => "home#top"
   get "about" => "home#about"
   get "posts/:id/show" => "posts#show"
